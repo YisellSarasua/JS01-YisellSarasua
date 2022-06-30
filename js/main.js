90d52c311d7e11c05a190157870545da6ad2164f
Yisell Sarasua <yisell.sarasua@gmail.com>
2022-06-30 10:39:43 -0300

    creo repositorio

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log('enviar mensaje en consola');
+console.log('enviar mensaje en consola 2');
+
+const Saludo = "Hola";
+let SegundoSaludo = 'chau';
+console.log(Saludo);
+console.log(SegundoSaludo);
